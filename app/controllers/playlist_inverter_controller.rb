9fc317e5f27c28fbac5c01f6c76d80387e10bb9c
Paul Forti <paul.forti@protonmail.com>
2017-02-05 07:18:12 -0600

    Added url checks

--- app/controllers/playlist_inverter_controller.rb
+++ app/controllers/playlist_inverter_controller.rb
@@ -6,17 +6,22 @@ class PlaylistInverterController < ApplicationController
     @code = params[:code]
   end
   def invert_now
-    Yt.configure do |config| config.log_level = :debug end
-    @account = Yt::Account.new authorization_code: params[:playlist_data][:code], redirect_uri: "#{ENV["BASE_REDIRECT_URI"]}/invert"
-    playlist = Yt::Playlist.new url: params[:playlist_data][:playlist_url]
-    ids = Array.new
-    @inverted_playlist = @account.create_playlist title: "#{playlist.title} inverted by #{ENV["BASE_REDIRECT_URI"]}"
-    playlist.playlist_items.each do |item|
-      ids.push item.video.id
-    end
+    match_pl = /playlist\?list=.+/.match(params[:playlist_data][:playlist_url])
+    if !match_pl
+      flash[:error] = "The format of the playlist url is incorrect"
+      render :index
+    else
+      @account = Yt::Account.new authorization_code: params[:playlist_data][:code], redirect_uri: "#{ENV["BASE_REDIRECT_URI"]}/invert"
+      playlist = Yt::Playlist.new url: params[:playlist_data][:playlist_url]
+      ids = Array.new
+      @inverted_playlist = @account.create_playlist title: "#{playlist.title} inverted by #{ENV["BASE_REDIRECT_URI"]}"
+      playlist.playlist_items.each do |item|
+        ids.push item.video.id
+      end
 
-    ids.reverse.each do |id|
-      @inverted_playlist.add_video id
+      ids.reverse.each do |id|
+        @inverted_playlist.add_video id
+      end
     end
   end
 end
